23fe2aa32646dee8695c3025ccbfd2d343b5a409
D Kwan <16279847+kwand@users.noreply.github.com>
2023-05-07 06:39:28 -0400

    Fix missing comma

--- lua/nerd-galaxyline.lua
+++ lua/nerd-galaxyline.lua
@@ -12,7 +12,7 @@ local themes_match = {
     gruvbox = "#261C00",
     everforest = "#282E2C",
     deus = "#16191D",
-    material = "#202D2F" -- https://github.com/marko-cerovac/material.nvim
+    material = "#202D2F", -- https://github.com/marko-cerovac/material.nvim
     tokyonight = "#24283B" -- https://github.com/folke/tokyonight.nvim
 }
 
